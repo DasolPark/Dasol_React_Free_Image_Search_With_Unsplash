ab16c7f80cd655fb4a3dfb00df2d27d4b94d346f
Dasol Park <dasol.park615@gmail.com>
2020-02-19 17:49:44 +0900

    ImageCard gridRowEnd with React.createRef()

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,12 +1,32 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { spans: 0 };
+
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.imageRef.current.addEventListener('load', this.setSpans);
+  }
+
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+
+    const spans = Math.ceil(height / 10);
+
+    this.setState({ spans: spans });
+  };
+
   render() {
     const { urls, alt_description } = this.props.image;
 
     return (
-      <div>
-        <img src={urls.regular} alt={alt_description} />
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+        <img ref={this.imageRef} src={urls.regular} alt={alt_description} />
       </div>
     );
   }
