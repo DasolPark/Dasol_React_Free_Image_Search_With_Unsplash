e99e590c0db421b8f885f819745c58c1bcc5f455
Dasol Park <dasol.park615@gmail.com>
2020-02-19 17:13:55 +0900

    Communicating Child to Parent

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,13 @@ import React from 'react';
 import SearchBar from './SearchBar';
 
 const App = () => {
+  function onSearchSubmit(term) {
+    console.log(term);
+  }
+
   return (
     <div className="ui container" style={{ marginTop: '10px' }}>
-      <SearchBar />
+      <SearchBar onSubmit={onSearchSubmit} />
     </div>
   );
 };
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,11 +3,11 @@ import React from 'react';
 class SearchBar extends React.Component {
   state = { term: '' };
 
-  onFormSubmit(e) {
+  onFormSubmit = e => {
     e.preventDefault();
 
-    console.log(this.state.term);
-  }
+    this.props.onSubmit(this.state.term);
+  };
 
   render() {
     return (
