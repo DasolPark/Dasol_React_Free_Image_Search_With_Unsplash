486cda2fee8c676a465aca725a15aa3d8898664a
Dasol Park <dasol.park615@gmail.com>
2020-02-19 17:30:52 +0900

    Get images with axios from unsplash

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,31 @@
 import React from 'react';
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
 
-const App = () => {
-  function onSearchSubmit(term) {
-    console.log(term);
-  }
+class App extends React.Component {
+  state = { images: [] };
 
-  return (
-    <div className="ui container" style={{ marginTop: '10px' }}>
-      <SearchBar onSubmit={onSearchSubmit} />
-    </div>
-  );
-};
+  onSearchSubmit = async term => {
+    try {
+      const response = await unsplash.get('/search/photos/', {
+        params: { query: term, per_page: 30 }
+      });
+
+      console.log(response);
+      this.setState({ images: response.data.results });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: '10px' }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+        Found {this.state.images.length} images
+      </div>
+    );
+  }
+}
 
 export default App;
